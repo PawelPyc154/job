f4dc45ebceaeb7d8c1c49c736bdf3d932cf88b9b
Paweł Pyc <pawelpyc154@gmail.com>
2022-05-06 19:09:29 +0200

    remove all

--- packages/ui/components/button/button.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-
-interface ButtonProps{children:ReactNode}
-export const Button =({ children }:ButtonProps)=> <button style={{color:'red'}}>{children}</button>
